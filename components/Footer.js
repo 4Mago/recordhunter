406df79ace197ddfac69490c1ddcfacaa726e725
4mago <marcus.bjorsson@gmail.com>
2022-08-05 14:48:41 +0200

    install styled components

---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Footer = () => {
+    return (
+        <>
+            <Div>
+
+            </Div>
+
+        </>
+    )
+}
+
+const Div = styled.div`
+    background-color: #ffe81C;
+    height: 150px;
+    width: 100%;
+`
+
+export default Footer
