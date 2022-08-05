ade494e1ad25d4d8c6a3c4bc7e7c18a80dfc29ab
4mago <marcus.bjorsson@gmail.com>
2022-08-05 16:12:03 +0200

    hej

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,9 @@ import styled from 'styled-components'
 const Footer = () => {
     return (
         <>
-            <Div>
 
+            <Div>
+                <LP src='/bilder/spinning-logo.png' />
             </Div>
 
         </>
@@ -13,9 +14,36 @@ const Footer = () => {
 }
 
 const Div = styled.div`
+    position: relative;
     background-color: #ffe81C;
     height: 150px;
     width: 100%;
+`
+
+const LP = styled.img`
+    position: absolute;
+    background-color: #1F1C1C;
+    left: 38%;
+    transform: translate(-50%, -50%);
+    top: -75%;
+    opacity:0.85;
+    max-height: 450px;
+    max-width: 100%;
+    object-fit: contain;
+    border-radius: 100%;
+    z-index: -5;
+    overflow: hidden;
+    animation: App-logo-spin infinite 20s linear;
+
+    @keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
 `
 
 export default Footer
