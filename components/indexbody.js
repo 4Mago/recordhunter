297879fd8443839245591635b4e87e81bac4de54
4mago <marcus.bjorsson@gmail.com>
2022-08-12 15:58:17 +0200

    enitaget

--- components/indexbody.js
+++ components/indexbody.js
@@ -1,34 +1,34 @@
 import React from 'react'
-import styled from 'styled-components'
+
 
 const Indexbody = () => {
   return (
       <>
-          <Container>
+          <div classname="container">
 
         
 
               
 
-              <Img  src='/bilder/index-background.png' />
+              <img className='index-bakgrund'  src='/bilder/index-background.png' />
 
 
-              <Div>
+              <div className='textbakgrund-container'>
 
-              <Textbackground>
+              <div className='textbakgrund'>
               Köpa LP skivor i Stockholm? <br/><br/>
 
                 Vår skivbutik har ett utmärkt urval av vinyl, CD och DVD. Vi har ny och begagnad musik i alla format, samt sällsynta fynd. Om du har ett favoritband eller letar efter något nytt att upptäcka har vi en enorm samling att välja mellan.
 
-                </Textbackground>
+                </div>
 
              
-              </Div>
+              </div>
               
               
           
 
-          </Container>
+          </div >
 
           
 
@@ -36,44 +36,8 @@ const Indexbody = () => {
   )
 }
 
-const Container = styled.div`
-   position: relative; 
-`
-
-const Div = styled.div`
-   display:flex;
-   justify-content:center;
-   align-items:center; 
-`
 
 
-const Img = styled.img`
-  height:100vh;
-  width:100%;
-  display:block;
-  margin:0;
-`
-
-const Textbackground = styled.div`
-position: absolute; 
-bottom: 30%; 
-background: rgb(0, 0, 0);
-background: rgba(0, 0, 0, 0.5); 
-width: 50%;
-opacity:1;
-color: white;
-font-size: 20px;
-padding: 50px;
-text-align: center;
-border-radius: 88px;
-  
-`
-
-
-
-
-
-  
 
 
 export default Indexbody
